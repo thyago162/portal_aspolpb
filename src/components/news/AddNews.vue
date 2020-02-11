733858ac8ade40fa4cedd31344ada95d055850a7
Thyago Lopes <thyago162@gmail.com>
2020-02-11 09:54:34 -0300

    Removendo imagens

--- src/components/news/AddNews.vue
+++ src/components/news/AddNews.vue
@@ -1,19 +1,29 @@
 <template>
     <b-modal title="Nova noticia" 
-        ref="news" id="news" size="xl">
-        <form >
+        ref="news" id="news" size="xl" header-bg-variant="primary">
+        <form  >
+            {{htmlForEditor}}
             <b-container fluid>
                 <b-row>
-                    <b-col >
+                    <b-col lg="9" md="7">
                         <b-form-group label="Título">
-                            <b-form-input type="text"></b-form-input>
+                            <b-form-input type="text" v-model="form.title" required></b-form-input>
+                        </b-form-group>
+                    </b-col>
+
+                    <b-col lg="3" md="2">
+                        <b-form-group label="Data">
+                            <b-form-input type="date" v-model="form.date" />
                         </b-form-group>
                     </b-col>
                 </b-row>
                 <b-row>
                     <b-col>
                         <b-form-group label="Conteúdo">
-                            <vue-editor v-model="content" useCustomImageHandler/>
+                            <vue-editor id="editor" :useCustomImageHandler="customHandle" 
+                                @image-added="handleImageAdded" v-model="form.content"
+                                @image-removed="handleImageDeleted"
+                                ></vue-editor>
                         </b-form-group>
                     </b-col>
                 </b-row>
@@ -24,18 +34,93 @@
 </template>
 
 <script>
-    import { VueEditor} from 'vue2-editor';
+    import { VueEditor } from 'vue2-editor';
+    
     export default {
         components: {
             VueEditor
         },
 
         computed: {
-         
+            token() {
+                return this.$session.get('jwt');
+            }
         },
         data() {
             return {
-                content: ''
+                form: {
+                    content: '',
+                    title: '',
+                    data: ''
+                },
+                customHandle: true,
+                erro: [],
+                validation: {}
+            }
+        },
+
+        methods: {
+            
+            handleImageAdded(file, Editor, cursorLocation) {
+
+                var data = new FormData();
+                data.append('image',file);
+
+                this.$http.post('news/image/add',data, {
+                    headers: {
+                        Authorization: 'Bearer '+this.token
+                    }
+                })
+                .then(res => {
+                    let url = res.data.url.replace('public/','storage/');
+                    Editor.insertEmbed(cursorLocation, "image", url);
+                })
+                .catch(err => {
+                    this.erro.push(err)
+                })
+            },
+
+            handleImageDeleted(file) {
+
+                let data = new FormData();
+                data.append('image',file.replace('http://localhost:8000/storage/news/',''))
+
+                this.$http.post('news/image/delete',data, {
+                    headers: {
+                        Authorization: 'Bearer '+this.token
+                    }
+                })
+                .then(res => {
+                    alert(res.data)
+                })
+                .catch(err => {
+                    this.erro.push(err);
+                })
+            },
+
+            handleOk: function(bvModalEvt) {
+                bvModalEvt.preventDefault();
+                this.handleImageAdded();
+            },
+
+            formSubmited() {
+
+            },
+
+            formValidation() {
+                
+            },
+
+            validTitle() {
+                return this.form.title > 0 ? 
+                this.validation.title = true : 
+                this.validation.title = false
+            },
+
+            validateContent() {
+                return this.form.content > 0 ?
+                this.validation.content = true :
+                this.validation.title = false
             }
         }
         

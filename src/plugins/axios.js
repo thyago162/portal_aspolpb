80b81500c76a50ce17a3ba9fa174b5df23ebf423
Thyago Lopes <thyago162@gmail.com>
2020-03-25 23:49:05 -0300

    Alterando endereço para produção

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.baseURL = 'http://ec2-18-229-134-125.sa-east-1.compute.amazonaws.com/api/'
 export default Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios

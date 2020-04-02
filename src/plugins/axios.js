6a68926e8ba0ea60e36e2c32876a8b3129cb5311
Thyago Lopes <thyago162@gmail.com>
2020-04-01 21:31:37 -0300

    Mudando para produção

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/'
+axios.defaults.baseURL = 'http://193.46.198.137:8000/api/'
 export default Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios

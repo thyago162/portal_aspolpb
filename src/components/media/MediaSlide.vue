48070bd81151c1dcbd7763f02d3ccebb4e51818f
Thyago Lopes <thyago162@gmail.com>
2020-06-21 11:32:09 -0300

    Restilizando

--- src/components/media/MediaSlide.vue
+++ src/components/media/MediaSlide.vue
@@ -1,113 +1,132 @@
 <template>
-    <div>
-        <carousel :perPageCustom="customSlide" class="ml-2 mt-4 mb-3">
-            <slide v-for="(media, index) in medias" :key="index" class="mr-2 box" >
+  <div>
+    <carousel :perPageCustom="customSlide" class="ml-2 mt-4 mb-3">
+      <slide v-for="(media, index) in medias" :key="index" class="mr-2 box">
+        <div v-if="media.nu_type === 1">
+          <b-link @click="redirectTo(media.nm_link)">
+            <div class="media-title">
+              <p>{{media.nm_title}}</p>
+            </div>
+          </b-link>
 
-                <div v-if="media.nu_type === 1">
-                    <b-link @click="redirectTo(media.nm_link)">
-                        <b-img class="image" fluid :src="media.nm_image_path" ></b-img>
-                    </b-link>
-                   
-                </div>
+          <b-img class="image" fluid :src="media.nm_image_path"></b-img>
+        </div>
 
-                <div v-if="media.nu_type === 2"  >
-                    <b-embed type="iframe"  :src="media.nm_link"></b-embed>
-                </div>
-            
-                <div v-if="media.nu_type === 3" >
-                
-                    <b-link v-b-modal.play-sound @click="playSound(media.nm_audio_path)"> 
-                        <b-icon icon="headphones" class="h1 icon"></b-icon>
-                    </b-link>
-                    <b-img class="image" fluid :src="media.nm_image_path" ></b-img>
-                    
-                </div>
+        <div v-if="media.nu_type === 2">
+          <b-embed type="iframe" :src="media.nm_link"></b-embed>
+        </div>
 
-                <div v-if="media.nu_type === 4">
-                    <b-link @click="redirectTo(media.nm_link)">
-                        <b-img-lazy class="image" fluid :src="media.nm_image_path"></b-img-lazy>
-                    </b-link>
+        <div v-if="media.nu_type === 3">
+          <b-link v-b-modal.play-sound @click="playSound(media.nm_audio_path)">
+            <b-icon icon="headphones" class="h1 icon"></b-icon>
+          </b-link>
+          <b-img class="image" fluid :src="media.nm_image_path"></b-img>
+        </div>
 
-                </div>
-            </slide>
-            
-        </carousel>
-        <AudioPlay :item="item"  />
-    </div>
+        <div v-if="media.nu_type === 4">
+          <b-link @click="redirectTo(media.nm_link)">
+            <div class="media-title">
+              <p>{{media.nm_title }}</p>
+            </div>
+          </b-link>
+
+          <b-img-lazy class="image" fluid :src="media.nm_image_path"></b-img-lazy>
+        </div>
+      </slide>
+    </carousel>
+    <AudioPlay :item="item" />
+  </div>
 </template>
 
 <script>
-    import AudioPlay from './AudioPlay';
-    export default {
+import AudioPlay from "./AudioPlay";
+export default {
+  props: ["medias"],
 
-        props: ['medias'],
+  components: {
+    AudioPlay
+  },
 
-        components: {
-            AudioPlay
-        },
+  data() {
+    return {
+      item: "",
+      customSlide: [
+        [1360, 4],
+        [1000, 3],
+        [800, 2],
+        [500, 1],
+        [300, 1]
+      ]
+    };
+  },
 
-        data() {
-            return {
-                item: '',
-                customSlide: [[1360,4], [1000, 3], [800,2], [500,1], [300,1]]
-            
-            }
-        },
+  methods: {
+    redirectTo(link) {
+      link.search("http") > 0
+        ? window.open(link)
+        : window.open("http://" + link);
+    },
 
-        methods: {
-            redirectTo(link) {
-                link.search('http') > 0 ?
-                window.open(link) :
-                window.open('http://'+link)
-            },
-
-            playSound (media) {
-                this.item = media.replace('public','storage');
-            }
-        }
-        
+    playSound(media) {
+      this.item = media.replace("public", "storage");
     }
+  }
+};
 </script>
 
 <style scoped>
-    .image {
-        width: 100%;
-        height: 180px;
-        object-fit: cover;
-        object-position: center;
-    }
+.image {
+  width: 100%;
+  height: 250px;
+  object-fit: cover;
+  object-position: center;
+}
 
-    span {
-        font-size: 16px;
-        font-weight: bolder;
-    }
-    h5 {
-        font-weight: bolder;
-        font-size: 17px;
-    }
+span {
+  font-size: 16px;
+  font-weight: bolder;
+}
+h5 {
+  font-weight: bolder;
+  font-size: 17px;
+}
 
-    .audio-play:hover {
-        color: green;
-    }
+.audio-play:hover {
+  color: green;
+}
 
-    .box {
-        border: 2px solid #333;
-    }
+.box {
+  border: 2px solid #333;
+}
 
-    .icon {
-        position: absolute;
-        z-index: 1000;
-        margin-top: 80px;
-        margin-left: 150px;
-        color: #ffffff;
-        background-color: rgba(51, 51, 51, 0.342);
-        border-radius: 5px;
-    }
+.icon {
+  position: absolute;
+  z-index: 1000;
+  margin-top: 80px;
+  margin-left: 150px;
+  color: #ffffff;
+  background-color: rgba(51, 51, 51, 0.342);
+  border-radius: 5px;
+}
 
-    .icon:hover {
-        background-color: rgb(48, 163, 48);
-    }
+.icon:hover {
+  background-color: rgb(48, 163, 48);
+}
 
-    
-    
+.media-title {
+  width: 325px;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  bottom: 0px;
+  background-color: rgba(51, 51, 51, 0.73);
+  color: #fff;
+  font-size: 14px;
+}
+
+.media-title:hover {
+  font-weight: bolder;
+}
 </style>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const default_url = 'http://192.168.122.72:8000/';
-//const default_url = 'http://193.46.198.137:8000/api';
+//const default_url = 'http://localhost:8000/api';
+const default_url = 'http://193.46.198.137:8000/api';
 
 export  { default_url };

83c1083737797f6c71049f31b5116ff8245ea502
Thyago Lopes <thyago.lbrasileiro@gmail.com>
2020-08-13 13:11:42 -0300

    Alterando para ssl

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //const default_url = 'http://localhost:8000/api';
-const default_url = 'http://193.46.198.137:8000/api';
+const default_url = 'https://193.46.198.137:8000/api';
 
 const validate = (param) => {
     if (param == undefined) {
